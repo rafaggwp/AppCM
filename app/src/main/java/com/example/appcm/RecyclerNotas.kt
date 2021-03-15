289cb81380d94094a1ac2cdf8ba5377347f0d86b
Diogo Rocha <diogorafalr98@gmail.com>
2021-03-15 23:44:00 +0000

    recyc

--- app/src/main/java/com/example/appcm/RecyclerNotas.kt
+++ app/src/main/java/com/example/appcm/RecyclerNotas.kt
@@ -2,11 +2,20 @@ package com.example.appcm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appcm.dataclasses.Nota
 
 class RecyclerNotas : AppCompatActivity() {
+
+
+    private lateinit var myList: ArrayList<Nota>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recycler_notas)
+        setContentView(R.layout.recycler_notas)
+
+
+        myList = ArrayList<Nota>()
+
 
         //
     }
---
+++ app/src/main/java/com/example/appcm/dataclasses/Nota.kt
@@ -0,0 +1,8 @@
+package com.example.appcm.dataclasses
+
+import java.util.*
+
+data class Nota(val title: String,
+                val description: String,
+                val date: Date
+                )
