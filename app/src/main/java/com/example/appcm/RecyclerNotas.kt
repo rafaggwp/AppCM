272194cd82e3d0fd96fcf5eb66fc21de738f148b
Diogo Rocha <diogorafalr98@gmail.com>
2021-03-15 22:09:06 +0000

    layout recycler e

--- app/src/main/java/com/example/appcm/RecyclerNotas.kt
+++ app/src/main/java/com/example/appcm/RecyclerNotas.kt
@@ -7,5 +7,7 @@ class RecyclerNotas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_notas)
+
+        //
     }
 }
